9a3fdf823500fa0349076820a51336d381f97e1d
gbrooksman <gbrooksman@gmail.com>
2020-09-01 15:26:56 -0400

    moe with lines

---
+++ Client/Entities/LineChart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SectorModel.Client.Entities
+{
+    public class Point
+    {
+        public Point()
+        {
+
+        }
+
+        public decimal X { get; set; }
+
+        public decimal Y { get; set; }
+
+    }
+
+
+    public class Line
+    {
+        public Line()
+        {
+
+        }
+
+        public List<Point> Points { get; set; }
+
+        public int ColorIndex { get; set; }  
+
+        public string Symbol { get; set; }
+
+        public string Title { get; set; }
+
+        public string Path { get; set; }
+
+    }
+}
